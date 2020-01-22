af84202119ae41ed1ed29eff1af4aeac0c357dad
Jayson Kesterson <jayson.kesterson24@gmail.com>
2020-01-22 17:44:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,9 +13,8 @@ class App < Sinatra::Base
   
    get "/say/:number/:phrase" do
     @number = params[:number].to_i
-    @number.times do
-     "#{params[:phrase]}"
-    end
+    @phrase = params[:phrase]
+    "#{@phrase} * #{@number}"
   end
   
    get "/say/:word1/:word2/:word3/:word4/:word5" do
